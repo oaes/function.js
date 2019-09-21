e8098f9d98a73793545f529f01993d658c65d182
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-09-21 08:46:28 +0600

    added a file

---
+++ function_invoke.js
@@ -0,0 +1,19 @@
+//javascript function invoke
+
+
+//add this two var
+function date(){
+    var a = 90
+    var h = 88
+    console.log(a+h)
+}
+date()
+
+
+//subtract this two var
+function name(){
+    var t = 70
+    var d = 60
+    console.log(t-d)
+}
+name()
